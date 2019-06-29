ef5d0db132ab7dcffc826cfbb3a807ecd9997780
Mitali Ahir <mitaliahir3@gmail.com>
2019-06-29 12:23:58 -0400

    Initial commit

---
+++ SimpleCommunication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCommunication
+//
+//  Created by Harshu Patel on 2019-06-29.
+//  Copyright © 2019 Mitali Ahir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
